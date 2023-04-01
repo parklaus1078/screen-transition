6dd8fc2039fc23081c199e804cb0c94ab36cc5d0
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2023-04-01 20:00:42 +0900

    [ADD] view life cycle tutorial

--- ScreenTransitionExample/CodePresentViewController.swift
+++ ScreenTransitionExample/CodePresentViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+protocol SendDataDelegate2: AnyObject {
+    func sendData(name: String)
+}
+
 class CodePresentViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     
     @IBAction func tapBackButton(_ sender: UIButton) {
+        self.delegate?.sendData(name: "Song")
         self.presentingViewController?.dismiss(animated: true)
     }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    weak var delegate: SendDataDelegate2?
 }
